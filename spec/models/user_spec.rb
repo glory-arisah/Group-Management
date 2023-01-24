c072689b2aa124596a1acdc26df809362e846bad
Glory Arisah <gloryarisah@gmail.com>
2023-01-24 16:08:10 +0100

    initial tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context "validations" do
+
+  end
+
+  context "associations" do
+
+  end
+
+  context "data persists" do
+    
+  end
+  
+
+end
