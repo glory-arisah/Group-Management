cad8728c2a8ca946975862d8a775d543690831ee
Glory Arisah <gloryarisah@gmail.com>
2023-01-26 22:16:07 +0100

    created and updated user system specs updated devise to accept username and fullname parameters updated the database and sign up form to accept username and fullname parameters created a support file to share common functions amongst system specs configured system spec in rails_helper.rb updated certain view files to make user system specs pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  # get "welcome/index"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  authenticated :user do
+    root "groups#index", as: :authenticated_root
+  end
+
+  root "welcome#index", as: :unauthenticated_root
+
+  resources :groups
 end
