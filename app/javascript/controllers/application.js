bcf571921f9d438bfe08671f19e0f82c5ea684ab
Glory Arisah <gloryarisah@gmail.com>
2023-03-23 18:19:56 +0100

    added styling for the authentication and landing page Enabled Sass for the project

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,8 +2,24 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
+let burgerIcon = document.querySelector('.fa-bars')
+
+const showLinks = () => {
+  let navLinks = document.querySelector('#nav-links')
+  if (navLinks.style.display === 'block') {
+    navLinks.style.display = 'none'
+  } else {
+    navLinks.style.display = 'block'
+  }
+}
+
+document.addEventListener('turbo:load', () => {
+  burgerIcon.addEventListener('click', showLinks)
+})
+
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
-export { application }
+
+export { application}
